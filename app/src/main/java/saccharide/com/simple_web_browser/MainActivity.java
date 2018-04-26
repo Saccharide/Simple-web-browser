8e32b22852cc9adccb9189aa216e347e93ba6835
saccharide <rtang@email.wm.edu>
2018-04-26 18:37:28 -0400

    Added input stream and buffer reader to try to read from server, will implement a async task later

--- app/src/main/java/saccharide/com/simple_web_browser/MainActivity.java
+++ app/src/main/java/saccharide/com/simple_web_browser/MainActivity.java
@@ -1,5 +1,6 @@
 package saccharide.com.simple_web_browser;
 
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,9 +47,48 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void send_web_request(String url, TextView user_view){
+    public void send_web_request(String user_input_url, TextView user_view){
 
-        user_view.setText(url);
+        String content = "Trying to connect to: " + user_input_url + "\n";
+        HttpURLConnection connection = null;
+        BufferedReader bufferedReader = null;
+        try {
+            URL url = new URL(user_input_url);
+            // Trying to oonnect to url;
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            // Get url's response
+            InputStream inputStream = connection.getInputStream();
+
+            // Create a buffer to read the input stream
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while ((line = bufferedReader.readLine())!= null){
+                stringBuffer.append(line);
+            }
+            content += stringBuffer.toString();
+
+            user_view.setText(content);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                // Close our connection and resources
+                if (connection != null)
+                    connection.disconnect();
+            try {
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
